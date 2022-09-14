22d221eae86c2a44500dfff35a2187c7efba719e
Deividas Rimkus <8516648+Lisviks@users.noreply.github.com>
2022-09-14 18:33:23 +0100

    js

---
+++ main.js
@@ -0,0 +1,12 @@
+const adviceIdSpan = document.querySelector('.advice-id');
+const adviceSpan = document.querySelector('.advice');
+const diceBtn = document.querySelector('.dice');
+
+diceBtn.addEventListener('click', () => {
+  fetch('https://api.adviceslip.com/advice')
+    .then((res) => res.json())
+    .then((data) => {
+      adviceIdSpan.innerText = data.slip.id;
+      adviceSpan.innerText = data.slip.advice;
+    });
+});
